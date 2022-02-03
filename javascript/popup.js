8d30172b1de5b89e86dc2f9a7c62cd2e4540b17c
Lithium/Helium <lithiumturkin@protonmail.com>
2022-02-03 13:48:17 +0100

    yejs

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,3 +1,7 @@
 window.onpointermove = function () {
     window.open('https://media.istockphoto.com/photos/office-nerd-picture-id172159131','My Window','height= 680px,width=460px ,');
 };
+
+window.onpointermove = function () {
+    window.open('https://jabbascriptproject.github.io/JabbaScript/img/bing chilling.png', 'bin chillin')
+};
