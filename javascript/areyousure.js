7bed81823da70bbeeca23782d9428c760a0bf842
Lithium/Helium <lithiumturkin@protonmail.com>
2022-03-08 13:47:31 +0100

    add disclaimer

---
+++ javascript/areyousure.js
@@ -0,0 +1,14 @@
+let cont = document.querySelector("#continue");
+let sure = false;
+
+cont.addEventListener('click', jegus);
+
+function jegus() {
+    if (sure == false) {
+        cont.innerHTML = "are you sure?";
+        sure = true;
+    } else {
+        window.open("https://irritant.nikirakoon.nl/");
+    }
+}
+
